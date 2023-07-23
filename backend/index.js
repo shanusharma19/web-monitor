b6a19ac6f90cded91bc51e7e018368c6115810a4
shanu sharma <shanu04012003@gmail.com>
2023-07-23 17:54:38 +0530

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/user", userRoutes);
+app.use(userRoutes);
 app.use(webRoutes);
 
 // const transport = nodemailer.createTransport({
